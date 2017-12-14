1d3e8bcc64a74ec7ae14210c05726f00b721f550
hth225 <hth225@naver.com>
2017-12-14 16:23:29 +0900

    learned func

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -13,7 +13,7 @@ if let name = optionalname,
         print(unwrappedOptional)
     }
 
-func hello(_ name : String, time : Int = 4) -> String {
+func hello(_ name : String, time : Int = 4) -> String { //파라미터 기본값 설정으로 파라미터 생략 가능
     var string = ""
     for _ in 0..<time {
         string += "\(name)님 안녕하세요!\n"
@@ -21,7 +21,7 @@ func hello(_ name : String, time : Int = 4) -> String {
     return string
 }
 
-func sum(_ numbers : Int ...) -> Int {
+func sum(_ numbers : Int ...) -> Int { // ... 으로 개수가 정해지지 않은 파라미터를 받음
     var sum = 0
     for number in numbers {
         sum += number
@@ -29,7 +29,7 @@ func sum(_ numbers : Int ...) -> Int {
     return sum
 }
 
-func hello(name : String, time : Int){
+func hello(name : String, time : Int){ //함수 안에 함수 작성
     func message(name : String) -> String{
         return "\(name)님 안녕하세요!"
     }
@@ -42,4 +42,17 @@ func hello(name : String, time : Int){
 print(hello("blogcin"))
 print(sum(3,5,7,8,4))
 hello(name: "op", time: 3)
+
+func hellogenerator(message : String) -> (String, String) -> String {
+    //문자열을 받아서 문자열을 반환 (여러개의 파라미터를 받는다면 (String, String)과 같은 형태 사용.
+    return {(fname : String, lname : String) -> String in
+        //중괄호로 감싼 실행가능한 코드 블럭, 클로저 in 키워드를 이용해서 파라미터, 반환타입영역과 실제 클로저 코드 영역을 분리.
+        return fname + lname + message
+        
+    }
+}
+
+let hey = hellogenerator(message: ", Greetings!")
+print(hey("lad", "neo"))
+
 
