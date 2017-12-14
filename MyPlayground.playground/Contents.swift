9cfa2ce270cde02a702bb36d8ecfb80b603661c6
hth225 <hth225@naver.com>
2017-12-14 13:58:27 +0900

    learned optional binding

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,8 +4,21 @@ import UIKit
 
 var optionalname : String? = "lad"
 var optionalemail : String? = "123@gmail.com"
+var unwrappedOptional : String! = "It's Unwrapped!"
 
 if let name = optionalname,
     let email = optionalemail {
         print(name, email)
+        print(optionalemail!)
+        print(unwrappedOptional)
     }
+
+func hello(_ name : String, time : Int = 4) -> String {
+    var string = ""
+    for _ in 0..<time {
+        string += "\(name)님 안녕하세요!\n"
+    }
+    return string
+}
+
+print(hello("blogcin"))
